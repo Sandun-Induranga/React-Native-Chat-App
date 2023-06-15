21d888f8f4acf3e523e7ba6dc5f1e4fb6a2accb3
Sandun Induranga <sandutharu40@gmail.com>
2023-06-15 10:48:02 +0530

    Login Screen Created

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
