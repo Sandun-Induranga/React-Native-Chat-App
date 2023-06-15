b7b5b5a1dbbbc08d311490cdecef4748e50525a5
Sandun Induranga <sandutharu40@gmail.com>
2023-06-15 10:57:48 +0530

    Tab Navigation

--- App.js
+++ App.js
@@ -4,17 +4,33 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import "react-native-gesture-handler";
+import Login from "./screens/Login";
 
 const Stack = createStackNavigator();
 const Tab = createMaterialTopTabNavigator();
 
+function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={AppNavigator} />
+      {/* <Tab.Screen name="Settings" component={Settings} /> */}
+    </Tab.Navigator>
+  );
+}
+
+function AppNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LoginScreen" component={Login} />
+      {/* <Stack.Screen name="Home" component={Home} /> */}
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <TabNavigator />
     </NavigationContainer>
   );
 }
