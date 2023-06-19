5a555ed40e32abb828ccf5a6add0fb027680b4ff
Sandun Induranga <sandutharu40@gmail.com>
2023-06-19 12:03:00 +0530

    Content Updated

--- components/ChatContext.js
+++ components/ChatContext.js
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import SendMessage from "./SendMessage";
 
 const ChatContext = () => {
   return (
     <View>
-      <Text>ChatContext</Text>
+      <SendMessage text="This is a message" />
     </View>
   );
 };
--- components/SendMessage.js
+++ components/SendMessage.js
@@ -1,12 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
-const SendMessage = () => {
+const SendMessage = (props) => {
   return (
-    <View>
-      <Text>SendMessage</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>{props.text}</Text>
     </View>
   );
 };
 
 export default SendMessage;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#003566",
+    alignItems: "center",
+    justifyContent: "start",
+    width: "max-content",
+    padding: 10,
+    borderRadius: 10,
+  },
+  text: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
